f8417a4ccafdb914c69e6cd00c96f0d433be163b
전지운 <wjsejdj@gmail.com>
2017-05-23 16:06:49 +0900

    Add files via upload

--- src/ds_10.py
+++ src/ds_10.py
@@ -1,8 +1,11 @@
+
 import pyspark
 myConf=pyspark.SparkConf()
-spark = pyspark.sql.SparkSession.builder.master("local").appName("myApp").config(conf=myConf).getOrCreate()
 
-wc=myRdd2.flatMap(lambda x:x.split()).map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False).take(10)
+spark = pyspark.sql.SparkSession.builder.master("local").appName("myApp").config(conf=myConf).getOrCreate()
+Rdd = spark.sparkContext.textFile(os.path.join('data','ds_spark_wiki.txt'));
+
+wc=Rdd.flatMap(lambda x:x.split()).map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0])).sortByKey(False).take(10)
 print type(wc)
 for i in wc:
     print i
